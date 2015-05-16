2b772915af7c9e9f9d37776e0a2139c7251ce7aa
unbelt <unbelt@outlook.com>
2015-05-16 21:09:22 +0300

    Initial Twitter app structure

---
+++ Twitter/Twitter.Web/App_Start/ViewEngineConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace Twitter.Web.App_Start
+{
+    using System.Web.Mvc;
+
+    public class ViewEngineConfiguration
+    {
+        internal static void RegisterViewEngines(ViewEngineCollection viewEngineCollection)
+        {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+        }
+    }
+}
---
+++ Twitter/Twitter.Web/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿namespace Twitter.Web
+{
+    using System.Web;
+    using System.Web.Http;
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+
+    using Twitter.Web.App_Start;
+
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            ViewEngineConfiguration.RegisterViewEngines(ViewEngines.Engines);
+
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
